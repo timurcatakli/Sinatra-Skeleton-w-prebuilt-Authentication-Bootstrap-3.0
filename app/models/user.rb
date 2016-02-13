04f0e8b8737867c998b73a686fe1a8fb255dc37f
Timur Catakli <timurcatakli@gmail.com>
2016-02-13 15:12:51 -0800

    Update app/models/users
    Added Email Validator class so that emails are validated through Regex... Sample usage is as follows:
    validates :email, presence: true, uniqueness: true, email: true

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, email: true
   validates :password_hash, presence: true
 
   def password
